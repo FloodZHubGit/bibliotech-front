c56610610961515bb539153b356b9eba0fffb707
FloodzHubGit <florentin.dekneudt@epsi.fr>
2023-03-08 09:17:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,108 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from './database.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1 class="text-3xl font-bold underline">
+      {{title}}
+    </h1>
+
+
+    <button (click)="createUser()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create user</button>
+
+    <button (click)="loginUser()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login user</button>
+
+    <button (click)="checkAuth()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Check auth</button>
+
+    <button (click)="logout()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+
+    <button (click)="addRecord()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add record</button>
+
+    <h2 class="text-2xl font-bold underline">
+      Records
+    </h2>
+  
+    <ul class="list-disc">
+      <li *ngFor="let record of records" class="text-xl"> {{record.titre}}       <button (click)="deleteRecord(record)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Delete</button> </li>
+    </ul>
+  `,
+  styles: []
 })
 export class AppComponent {
-  title = 'front';
+  title = 'Bibliotech';
+  records: any[] = [];
+
+  constructor(private databaseService: DatabaseService) {}
+
+  ngOnInit(): void {
+    this.databaseService.getRecords().then(records => {
+      this.records = records;
+      console.log('Records fetched:', records);
+    }).catch(err => {
+      console.log('Error fetching records:', err);
+    });
+  }
+
+  addRecord() {
+    this.databaseService.addRecord().then(record => {
+      console.log('Record created:', record);
+      //reload records
+      this.databaseService.getRecords().then(records => {
+        this.records = records;
+        console.log('Records fetched:', records);
+      }, err => {
+        console.log('Error fetching records:', err);
+      }
+      );
+    }).catch(err => {
+      console.log('Error creating record:', err);
+    });
+  }
+
+  createUser() {
+    this.databaseService.createUser().then(record => {
+      console.log('User created:', record);
+    }).catch(err => {
+      console.log('Error creating user:', err);
+    });
+  }
+
+  loginUser() {
+    this.databaseService.loginUser().then(record => {
+      console.log('User logged in:', record);
+    }).catch(err => {
+      console.log('Error logging in user:', err);
+    });
+  }
+
+  checkAuth() {
+    this.databaseService.checkAuth().then(record => {
+      console.log('User checked:', record);
+    }).catch(err => {
+      console.log('Error checking user:', err);
+    });
+  }
+
+  logout() {
+    this.databaseService.logoutUser();
+  }
+
+  deleteRecord(record : any) {
+    this.databaseService.deleteRecord(record.id).then(record => {
+      console.log('Record deleted:', record);
+      //reload records
+      this.databaseService.getRecords().then(records => {
+        this.records = records;
+        console.log('Records fetched:', records);
+      }, err => {
+        console.log('Error fetching records:', err);
+      }
+      );
+    }).catch(err => {
+      console.log('Error deleting record:', err);
+    });
+  }
+
+
 }
---
+++ src/app/database.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import PocketBase from 'pocketbase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  private pb: PocketBase;
+
+  constructor() {
+    this.pb = new PocketBase('http://127.0.0.1:8090');
+  }
+
+  async getRecords() {
+    let records = await this.pb.collection('tests').getFullList({
+      sort: '-created',
+    });
+    return records;
+  }
+
+  async addRecord() {
+    const data = {
+      "titre": "test"
+    };
+    
+    const record = await this.pb.collection('tests').create(data);
+  }
+
+  async createUser() {
+    const data = {
+      "username": "test_username",
+      "email": "test@example.com",
+      "emailVisibility": true,
+      "password": "12345678",
+      "passwordConfirm": "12345678",
+      "name": "test",
+    };
+
+    const record = await this.pb.collection('users').create(data);
+  }
+
+  //login user
+  async loginUser() {
+    const authData = await this.pb.collection('users').authWithPassword(
+      'test@example.com',
+      '12345678',
+    );
+  
+    // after the above you can also access the auth data from the authStore
+    console.log(this.pb.authStore.isValid);
+    console.log(this.pb.authStore.token);
+    console.log(this.pb.authStore.model?.id);
+  }
+
+  async checkAuth() {
+    //check if user is authenticated
+    if (this.pb.authStore.isValid) {
+      console.log('user is authenticated');
+    }
+    else {
+      console.log('user is not authenticated');
+    }
+  }
+
+  async logoutUser() {
+    await this.pb.authStore.clear();
+  }
+
+  async deleteRecord(id: string) {
+    await this.pb.collection('tests').delete(id);
+  }
+
+}
